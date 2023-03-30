084b355bba2be500cd24c3eb3583c2f6d3e4563a
Carolina Serpe <carolinaserpe@MacBook-Air-de-Carolina.local>
2023-03-30 19:14:42 -0300

    modificacion de footer, ahora aparece en todas las paginas con nuestros nombres, creamos carpeta data y pusimos un ejemplo

---
+++ data/data.js
@@ -0,0 +1,14 @@
+const  user = {
+    email: "manuel@gamil.com",
+    usuario: "ManuelGomez",
+    fechaNacimiento: 01/03/2001,
+    numeroTel: 11-4859-9234, 
+    fotoPerfil: "./data/default_pfp.png",
+    
+}
+// ver que onda si esto funciona o no --> tema de la imagen y como vincularla para que aparezca en las otras paginas que pide la consigna 
+
+const products = [product = {imagen: "./data/manta.jpeg", nombreProducto: "Manta Norwegian", descripcion: "Manta de 50x70 cm", fechaCarga: 04/03/2023,}]
+
+
+module.exports = user; 
