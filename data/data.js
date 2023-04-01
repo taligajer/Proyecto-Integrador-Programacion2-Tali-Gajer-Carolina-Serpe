0783e47440def358622c17ca7a8d575c8f3d64f0
taligajer <tgajer@udesa.edu.ar>
2023-04-01 15:44:46 -0300

    Agregue productos y comentarios a data.js

--- data/data.js
+++ data/data.js
@@ -12,8 +12,17 @@ const products = [{imagen: "./data/manta", nombreProducto: "Manta Norwegian", de
 "./data/termo.jpeg", nombreProducto: "Termo", descripcion: "Termo", fechaCarga: 23/09/2023}, {imagen: "./data/almohadon.jpeg",
 nombreProducto: "Almohadon", descripcion: "Almohadon", fechaCarga: 25/05/2023}, {imagen: "./data/mesa.jpeg", nombreProducto: "Mesa",
 descripcion: "Mesa de comedor", fechaCarga: 12/06/2023}, {imagen: "./data/silla.jpeg", nombreProducto: "Silla", descripcion: "Silla de comedor",
-fechaCarga: 09/06/2023} ]
+fechaCarga: 09/06/2023}, {imagen: "./data/sillon.jpg", nombreProducto: "Sillon", descripcion: "Sillon para living",
+fechaCarga: 08/09/2023}, {imagen: "./data/Cama.jpg", nombreProducto: "Cama", descripcion: "Cama de 2 plazas",
+fechaCarga: 28/02/2022}, {imagen: "./data/mesaratona.jpg", nombreProducto: "Mesa ratona", descripcion: "Mesa ratona para lampara",
+fechaCarga: 05/06/2023}, {imagen: "./data/Lampara.jpg", nombreProducto: "Lampara", descripcion: "Lampara de mesa",
+fechaCarga: 06/07/2023}, {imagen: "./data/mantel.jpg", nombreProducto: "Mantel", descripcion: "Mantel para mesa de comedor",
+fechaCarga: 07/12/2023}]
 
+const comentarios = [{nombreUsuario: "Manuel",
+    textoComentario: "Excelente producto",
+    imagenPerfil: "./data/Default_pfp.png"}]
 
 module.exports = user; 
 module.exports = products; 
+module.exports = comentarios;
