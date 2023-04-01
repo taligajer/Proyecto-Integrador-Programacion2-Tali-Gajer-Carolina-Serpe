f009ea49dd4b33776e504574961125e8fcdbbe89
caro-serpe <carolinaserpe@MacBook-Air-de-Carolina.local>
2023-04-01 14:58:14 -0300

    agregue productos en la parte de data.js

--- data/data.js
+++ data/data.js
@@ -7,8 +7,13 @@ const  user = {
     
 }
 // ver que onda si esto funciona o no --> tema de la imagen y como vincularla para que aparezca en las otras paginas que pide la consigna 
-
-const products = [product = {imagen: "./data/manta.jpeg", nombreProducto: "Manta Norwegian", descripcion: "Manta de 50x70 cm", fechaCarga: 04/03/2023,}]
+// faltan poner 5 productos mas y la parte de comentarios 
+const products = [{imagen: "./data/manta", nombreProducto: "Manta Norwegian", descripcion: "Manta de 50x70 cm", fechaCarga: 04/03/2023,}, {imagen:
+"./data/termo.jpeg", nombreProducto: "Termo", descripcion: "Termo", fechaCarga: 23/09/2023}, {imagen: "./data/almohadon.jpeg",
+nombreProducto: "Almohadon", descripcion: "Almohadon", fechaCarga: 25/05/2023}, {imagen: "./data/mesa.jpeg", nombreProducto: "Mesa",
+descripcion: "Mesa de comedor", fechaCarga: 12/06/2023}, {imagen: "./data/silla.jpeg", nombreProducto: "Silla", descripcion: "Silla de comedor",
+fechaCarga: 09/06/2023} ]
 
 
 module.exports = user; 
+module.exports = products; 
