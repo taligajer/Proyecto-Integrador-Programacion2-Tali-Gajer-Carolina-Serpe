11dea1f9e5b763aa96a164bcb9bf07f600ed2fc3
caro-serpe <carolinaserpe@MacBook-Air-de-Carolina.local>
2023-04-03 08:09:46 -0300

    puse los comentarios en datajs

--- data/data.js
+++ data/data.js
@@ -8,16 +8,16 @@ const  user = {
 }
 // ver que onda si esto funciona o no --> tema de la imagen y como vincularla para que aparezca en las otras paginas que pide la consigna 
 // faltan poner 5 productos mas y la parte de comentarios 
-const products = [{imagen: "manta.jpeg", nombreProducto: "Manta Norwegian", descripcion: "Manta de 50x70 cm", fechaCarga:'4/03/2023',}, {imagen:
-"termo.jpeg", nombreProducto: "Termo", descripcion: "Termo", fechaCarga: '23/09/2023', alt: "Termo"}, {imagen: "almohadon.jpeg",
-nombreProducto: "Almohadon", descripcion: "Almohadon", fechaCarga: '25/05/2023'}, {imagen: "mesa.jpeg", nombreProducto: "Mesa",
-descripcion: "Mesa de comedor", fechaCarga: '12/06/2023'}, {imagen: "silla.jpeg", nombreProducto: "Silla", descripcion: "Silla de comedor",
-fechaCarga:'9/06/2023'}, {imagen: "sillon.jpg", nombreProducto: "Sillon", descripcion: "Sillon para living",
-fechaCarga:'8/09/2023'}, {imagen: "Cama.jpg", nombreProducto: "Cama", descripcion: "Cama de 2 plazas",
-fechaCarga: '28/02/2022'}, {imagen: "mesaratona.jpg", nombreProducto: "Mesa ratona", descripcion: "Mesa ratona para lampara",
-fechaCarga:'5/06/2023'}, {imagen: "Lampara.jpg", nombreProducto: "Lampara", descripcion: "Lampara de mesa",
-fechaCarga:'6/07/2023'}, {imagen: "mantel.jpg", nombreProducto: "Mantel", descripcion: "Mantel para mesa de comedor",
-fechaCarga:'7/12/2023'}]
+const products = [{imagen: "manta.jpeg", nombreProducto: "Manta Norwegian", descripcion: "Manta de 50x70 cm", fechaCarga:'4/03/2023', comentarios: "Execelnte producto"}, {imagen:
+"termo.jpeg", nombreProducto: "Termo", descripcion: "Termo", fechaCarga: '23/09/2023', alt: "Termo", comentarios: "Excelente producto"}, {imagen: "almohadon.jpeg",
+nombreProducto: "Almohadon", descripcion: "Almohadon", fechaCarga: '25/05/2023', comentarios: "Excelente producto"}, {imagen: "mesa.jpeg", nombreProducto: "Mesa",
+descripcion: "Mesa de comedor", fechaCarga: '12/06/2023', comentarios: "Excelente producto"}, {imagen: "silla.jpeg", nombreProducto: "Silla", descripcion: "Silla de comedor",
+fechaCarga:'9/06/2023', comentarios: "Excelente producto"}, {imagen: "sillon.jpg", nombreProducto: "Sillon", descripcion: "Sillon para living",
+fechaCarga:'8/09/2023', comenarios: "Excelente producto"}, {imagen: "Cama.jpg", nombreProducto: "Cama", descripcion: "Cama de 2 plazas",
+fechaCarga: '28/02/2022', comentarios: "Excelente producto"}, {imagen: "mesaratona.jpg", nombreProducto: "Mesa ratona", descripcion: "Mesa ratona para lampara",
+fechaCarga:'5/06/2023', comentarios: "Excelente producto"}, {imagen: "Lampara.jpg", nombreProducto: "Lampara", descripcion: "Lampara de mesa",
+fechaCarga:'6/07/2023', comentarios: "Excelente producto"}, {imagen: "mantel.jpg", nombreProducto: "Mantel", descripcion: "Mantel para mesa de comedor",
+fechaCarga:'7/12/2023', comentarios: "Excelente producto"}]
 
 const comentarios = [{nombreUsuario: "Manuel",
     textoComentario: "Excelente producto",
