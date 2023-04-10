affe59aa48cc68c4e819f6a11d2006fc09b43023
taligajer <tgajer@udesa.edu.ar>
2023-04-10 08:19:54 -0300

    cambie el nombre del logo

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require('express'); // requiere express
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/index'); 
 var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/products');
 
