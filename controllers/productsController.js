63091cdbeb7e335003a793efcd11ef592138849f
agustinc3 <hcagnoli@udesa.edu.ar>
2023-06-11 14:37:56 -0300

    agregando comentarios en detalle y relaciones en el controlador

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,5 +1,7 @@
-const db = require('../database/models'); 
+const db = require('../database/models');
 const producto = db.Product;
+const usuario = db.Usuario;
+const comentario = db.Comentario;
 const datajs = require('../data/data');
 const productos = datajs.products;
 const user = datajs.user;
@@ -28,26 +30,31 @@ let op = db.Sequelize.Op
 
 
 const controller = {
-  
 
-  findOne: function(req, res, next) 
-  {
+
+  findOne: function (req, res, next) {
     let id = req.params.id;
-    let criterio = { where: { id: id } }
-    producto.findOne(criterio)
-    .then(function(data){
-      return res.render("product",{title:"Con findOne",data:[data]})
-      
-  })
-  .catch(function(err){console.log(err)})
-    
-  },
-  productAdd: function(req, res, next) {
-    const newProducts = productos;
-      res.render('product-add', { title: 'products', newProducts, productName: productos.nombreProducto, username: user[0].usuario });
-    },
+    let criterio = { 
+    where: { id: id },
+    include:[{ association: "comentario" },
+        { association: "usuarios" }],
+    order: [['createdAt', 'DESC']] 
+  }
 
-  procesarAdd: function(req, res, next) {
+    producto.findOne(criterio)
+      .then(function (data) {
+        return res.render("product", { title: "Con findOne", data: [data] })
+
+      })
+      .catch(function (err) { console.log(err) })
+
+  },
+  productAdd: function (req, res, next) {
+    const newProducts = productos;
+    res.render('product-add', { title: 'products', newProducts, productName: productos.nombreProducto, username: user[0].usuario });
+  },
+
+  procesarAdd: function (req, res, next) {
     let productadd = req.body
     //console.log(req.session.user.id);
     producto.create({
@@ -59,27 +66,28 @@ const controller = {
     })
     res.redirect('/')
   },
-  
-  buscador: function(req, res, next) {
-      
-      let busqueda = req.query.search
-    
-      let criterio = {
-        where: {
-         nombreProducto: {
-            [op.like]: "%"+busqueda+"%"
-          }
-        }
-      }; 
-      let relaciones = {
-        include: [
-          { association: "userRel" }
-        ]}
-      producto.findAll(criterio, relaciones)
-        .then(function(data){
-          res.render('search-results',{data:data})
-        })
-      }
+
+  buscador: function (req, res, next) {
+
+    let busqueda = req.query.search
+
+    let criterio = {
+      where: {
+        [op.or]: [
+          { nombreProducto: { [op.like]: "%" + busqueda + "%" } },
+          { descripcion: { [op.like]: "%" + busqueda + "%" } }
+        ]
+      },
+      include: 
+      [{association: "comentario"},
+      {association: "usuarios"}],
+      order:[['createdAt', 'DESC']]
+    } 
+    producto.findAll(criterio)
+      .then(function (data) {
+        res.render('search-results', { data: data })
+      })
+  }
 }
 
 module.exports = controller;
